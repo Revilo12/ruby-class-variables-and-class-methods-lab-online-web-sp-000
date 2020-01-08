2de6d4c5480041df84145b15990f1c8499dc4dbb
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-08 16:10:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,16 +2,29 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@genres = []
+  @@artists = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    add_unless(@@genres, genre)
+    add_unless(@@artists, artist)
+
+  end
+
+  def add_unless(class_variable, instance)
+    class_variable << instance unless class_variable.include?(instance)
   end
 
   def count
     @@count
+  end
+
+  def genres
+    @@genres
   end
 
 end
